d2798a1da92f861bc2c637a63261732a579094e8
Vasily Kazantsev <vecnas@gmail.com>
2026-01-31 22:08:59 +0300

    Use jiant.dom helpers in docsMain

--- js/app/docsMain.js
+++ js/app/docsMain.js
@@ -1,66 +1,5 @@
 jiant.module("docsMain", function({app, jiant}) {
-  function unwrapView(view) {
-    if (!view) {
-      return null;
-    }
-    return view._el || view.el || view;
-  }
-
-  function firstViewElem(view) {
-    const el = unwrapView(view);
-    if (!el) {
-      return null;
-    }
-    if (el.nodeType) {
-      return el;
-    }
-    if (Array.isArray(el)) {
-      return unwrapView(el[0]) || null;
-    }
-    if (typeof el.length === "number" && (typeof el.item === "function" || el[0])) {
-      return unwrapView(el[0]) || null;
-    }
-    return el;
-  }
-
-  function clearElem(view) {
-    const el = firstViewElem(view);
-    if (!el) {
-      return;
-    }
-    while (el.firstChild) {
-      el.removeChild(el.firstChild);
-    }
-  }
-
-  function addClass(view, cls) {
-    const el = firstViewElem(view);
-    if (el && el.classList) {
-      el.classList.add(cls);
-    }
-  }
-
-  function removeClass(view, cls) {
-    const el = firstViewElem(view);
-    if (el && el.classList) {
-      el.classList.remove(cls);
-    }
-  }
-
-  function appendTo(target, node) {
-    const targetEl = firstViewElem(target);
-    const nodeEl = unwrapView(node);
-    if (targetEl && nodeEl) {
-      targetEl.appendChild(nodeEl);
-    }
-  }
-
-  function onClick(view, handler) {
-    const el = firstViewElem(view);
-    if (el) {
-      el.addEventListener("click", handler);
-    }
-  }
+  const dom = jiant.dom;
 
   jiant.onApp("jDocs", function(app) {
 
@@ -81,67 +20,66 @@ jiant.module("docsMain", function({app, jiant}) {
 
     function updateSelectedCtl(topic, subtopic, section) {
       showTopic(topic, subtopic, section);
-      currentTopic && topicCtls[currentTopic] && removeClass(topicCtls[currentTopic], "selected");
-      currentSubtopic && topicCtls[currentSubtopic] && removeClass(topicCtls[currentSubtopic], "selected");
+      currentTopic && topicCtls[currentTopic] && dom.removeClass(topicCtls[currentTopic], "selected");
+      currentSubtopic && topicCtls[currentSubtopic] && dom.removeClass(topicCtls[currentSubtopic], "selected");
       currentTopic = topic;
       currentSubtopic = subtopic;
-      topicCtls[currentTopic] && addClass(topicCtls[currentTopic], "selected");
-      currentSubtopic && topicCtls[subtopic] && addClass(topicCtls[subtopic], "selected");
+      topicCtls[currentTopic] && dom.addClass(topicCtls[currentTopic], "selected");
+      currentSubtopic && topicCtls[subtopic] && dom.addClass(topicCtls[subtopic], "selected");
     }
 
     function showTopic(topic, subtopic, section) {
       jiant.loadModule(app, subtopic, function() {
-        const containerEl = firstViewElem(app.views.main.container) || document;
-        containerEl.querySelectorAll("pre code").forEach(function(block) {
-          hljs.highlightBlock(block);
-        });
+        const containerEl = dom.first(app.views.main.container) || document;
+        if (containerEl.querySelectorAll) {
+          containerEl.querySelectorAll("pre code").forEach(function(block) {
+            hljs.highlightBlock(block);
+          });
+        }
         updateSubnav(section);
       }, true, app.views.main.container);
     }
 
     function updateSubnav(section) {
-      clearElem(app.views.main.subnav);
-      const navRoot = firstViewElem(app.views.nav);
+      dom.empty(app.views.main.subnav);
+      const navRoot = dom.first(app.views.nav);
       addSubnavItem(app.intl.onTop(), navRoot, 0);
-      const containerEl = firstViewElem(app.views.main.container);
+      const containerEl = dom.first(app.views.main.container);
       if (!containerEl) {
         return;
       }
-      Array.from(containerEl.querySelectorAll("h4")).forEach(function(elem, i) {
-        addSubnavItem(elem.innerHTML, elem, i);
-        if (i === section) {
-          if (typeof elem.scrollIntoView === "function") {
+      if (containerEl.querySelectorAll) {
+        Array.from(containerEl.querySelectorAll("h4")).forEach(function(elem, i) {
+          addSubnavItem(elem.innerHTML, elem, i);
+          if (i === section && typeof elem.scrollIntoView === "function") {
             elem.scrollIntoView({behavior: "smooth"});
           }
-        }
-      });
+        });
+      }
     }
 
     function addSubnavItem(label, elem, idx) {
       const v = app.templates.subnav.parseTemplate({label: label});
-      appendTo(app.views.main.subnav, v);
-      onClick(v, function() {
+      dom.append(app.views.main.subnav, v);
+      dom.on(v, "click", function() {
         app.states[""].go(undefined, undefined, idx, new Date().getTime());
       });
     }
 
     Object.entries(app.topics).forEach(function([topic, content]) {
       const navItem = app.templates.nav.parseTemplate({label: topic});
-      appendTo(app.views.nav, navItem);
-      onClick(navItem.ctl, function() {
+      dom.append(app.views.nav, navItem);
+      dom.on(navItem.ctl, "click", function() {
         app.states[""].go(topic, topic + "0", 0);
       });
       topicCtls[topic] = navItem;
       Object.entries(content).forEach(function([sub, subcontent]) {
         const subnav = app.templates.nav.parseTemplate({label: sub});
-        appendTo(navItem.container, subnav);
-        onClick(subnav.ctl, function() {
+        dom.append(navItem.container, subnav);
+        dom.on(subnav.ctl, "click", function() {
           app.states[""].go(topic, sub, 0);
         });
-        const subnavContainerEl = firstViewElem(subnav.container);
-        if (subnavContainerEl) {
-          subnavContainerEl.remove();
-        }
+        dom.remove(subnav.container);
         topicCtls[sub] = subnav;
         jiant.module(sub, {html: "html/doc/" + sub + ".html"});
       });
